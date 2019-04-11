07d940809ac1e07d04dcb21020da0042bf6317f1
Aaron Lun <lun.aaron@gene.com>
2019-04-11 16:58:35 -0700

    Split off basic test functions into setup.R.

--- tests/testthat/test-hash.R
+++ tests/testthat/test-hash.R
@@ -1,28 +1,5 @@
 # Tests the basic hashing capabilities for barcode searching.
-# library(gp.sa.screen); library(testthat); source("test-hash.R")
-
-BASES <- c("A", "C", "G", "T")
-
-GENERATE_RANDOM_SEQ <- function(n) {
-    paste(sample(BASES, n, replace=TRUE), collapse="")
-}
-
-MANUAL_HASH <- function(barcode, split=FALSE) {
-    if (split) {
-        B <- barcode
-    } else { 
-        B <- strsplit(barcode, "")[[1]]
-    }
-
-    collected <- numeric(0)
-    while (length(B)) {
-        word <- head(B, 16)
-        as_num <- c(A=0, C=1, G=2, T=3)[word] * 4^(seq_along(word)-1)
-        collected <- c(collected, sum(as_num))
-        B <- tail(B, -16)
-    }
-    collected
-}
+# library(gp.sa.screen); library(testthat); source("setup.R"); source("test-hash.R")
 
 test_that("basic hashing works correctly", {
     BASIC_COMPARE <- function(barcode) {
