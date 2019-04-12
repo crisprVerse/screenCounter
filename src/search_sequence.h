9f9e38cba648947c9bdcca56530b09634e325ab4
Aaron Lun <lun.aaron@gene.com>
2019-04-12 09:56:14 -0700

    Switch to std::array for greater efficiency.

--- src/search_sequence.h
+++ src/search_sequence.h
@@ -1,7 +1,11 @@
 #ifndef SEARCH_SEQUENCE_H
 #define SEARCH_SEQUENCE_H
 
+#include "Rcpp.h"
+
 #include <string>
+#include <array>
+
 #include "build_dictionary.h"
 #include "hash_sequence.h"
 
@@ -11,109 +15,105 @@ int is_valid (const char*, size_t);
 
 class hash_scanner {
 public:
+    hash_scanner() = default;
     hash_scanner(const char*, size_t);
     void advance ();
     const std::u32string& hash() const;
     bool valid() const;
 private:
-    const char* ptr;
-    size_t len;
+    const char* ptr = NULL;
+    size_t len = 0;
     std::u32string hashed;
-    size_t nvalid;
+    size_t nvalid = 0;
 };
 
 /* Sets up search information. */
 
 template<size_t nvariable>
 struct combination {
-    int indices[nvariable];
+    std::array<int, nvariable> indices;
 };
 
-template<size_t nvariable>
+template<size_t nvariable, size_t nconstant=nvariable+1>
 struct search_info {
-    search_info(Rcpp::List, Rcpp::StringVector, Rcpp::LogicalVector, Rcpp::LogicalVector);
-    std::vector<sequence_dictionary> perfect, deleted;
-    std::vector<std::u32string> constant_hash;
-    std::vector<int> constant_lengths, variable_lengths;
-    std::vector<size_t> constant_starts, variable_starts;
-    std::vector<size_t> nbarcodes;
+    search_info(Rcpp::List Guides, Rcpp::StringVector Constants,
+        Rcpp::LogicalVector allow_subs, Rcpp::LogicalVector allow_dels) 
+    {   
+        if (allow_subs.size()!=1) {
+            throw std::runtime_error("should be a logical scalar");
+        }
+        const bool allow_sub=allow_subs[0];
+        allow_del=allow_dels[0];
+
+        // Setting up the guides.
+        if (nvariable!=Guides.size()) {
+            std::stringstream err;
+            err << "expecting " << nvariable << " variable regions";
+            throw std::runtime_error(err.str());
+        }
+
+        for (size_t g=0; g<nvariable; ++g) {
+            Rcpp::StringVector guides(Guides[g]);
+            nbarcodes[g]=guides.size();
+            perfect[g]=build_dictionary(guides, allow_sub);
+            variable_lengths[g]=perfect[g].len;
+            if (allow_del) {
+                deleted[g]=build_deleted_dictionary(guides);
+            }
+        }
+
+        // Setting up the constant regions.
+        if (Constants.size()!=nconstant) {
+            throw std::runtime_error("number of constant regions should be 1 more than variable regions");
+        }
+
+        for (size_t s=0; s<nconstant; ++s) {
+            Rcpp::String con(Constants[s]);
+            const size_t len=Rf_length(con.get_sexp());
+            const char* ptr=con.get_cstring();
+            constant_hash[s]=hash_sequence(ptr, len); 
+            constant_lengths[s]=len;
+        }
+
+        // Setting up search parameters.
+        constant_starts[0]=0;
+        total_len=constant_lengths[0];
+        for (size_t i=0; i<nvariable; ++i) {
+            variable_starts[i]=total_len;
+            total_len+=variable_lengths[i];
+            constant_starts[i+1]=total_len;
+            total_len+=constant_lengths[i+1];
+        }
+
+        return;
+    }
+
+    std::array<sequence_dictionary, nvariable> perfect, deleted;
+    std::array<int, nvariable> variable_lengths;
+    std::array<size_t, nvariable> variable_starts;
+    std::array<size_t, nvariable> nbarcodes;
+
+    std::array<std::u32string, nconstant> constant_hash;
+    std::array<int, nconstant> constant_lengths;
+    std::array<size_t, nconstant> constant_starts;
+
     size_t total_len;
     bool allow_del;
 };
 
-template<size_t nvariable>
-search_info<nvariable>::search_info(Rcpp::List Guides, Rcpp::StringVector Constants,
-    Rcpp::LogicalVector allow_subs, Rcpp::LogicalVector allow_dels) 
-{
-    if (allow_subs.size()!=1) {
-        throw std::runtime_error("should be a logical scalar");
-    }
-    const bool allow_sub=allow_subs[0];
-    allow_del=allow_dels[0];
-
-    // Setting up the guides.
-    if (nvariable!=Guides.size()) {
-        std::stringstream err;
-        err << "expecting " << nvariable << " variable regions";
-        throw std::runtime_error(err.str());
-    }
-    perfect.reserve(nvariable);
-    deleted.reserve(nvariable);
-    nbarcodes.reserve(nvariable);
-    variable_lengths.reserve(nvariable);
-
-    for (size_t g=0; g<nvariable; ++g) {
-        Rcpp::StringVector guides(Guides[g]);
-        nbarcodes.push_back(guides.size());
-        perfect.push_back(build_dictionary(guides, allow_sub));
-        variable_lengths.push_back(perfect.back().len);
-        if (allow_del) {
-            deleted.push_back(build_deleted_dictionary(guides));
-        }
-    }
-
-    // Setting up the constant regions.
-    constexpr size_t nconstant=nvariable+1;
-    if (Constants.size()!=nconstant) {
-        throw std::runtime_error("number of constant regions should be 1 more than variable regions");
-    }
-
-    constant_hash.reserve(nconstant);
-    constant_lengths.reserve(nconstant);
-
-    for (size_t s=0; s<nconstant; ++s) {
-        Rcpp::String con(Constants[s]);
-        const size_t len=Rf_length(con.get_sexp());
-        const char* ptr=con.get_cstring();
-        constant_hash.push_back(hash_sequence(ptr, len)); 
-        constant_lengths.push_back(len);
-    }
-
-    // Setting up search parameters.
-    constant_starts.resize(nconstant);
-    variable_starts.resize(nvariable);
-    total_len=constant_lengths[0];
-
-    for (size_t i=0; i<nvariable; ++i) {
-        variable_starts[i]=total_len;
-        total_len+=variable_lengths[i];
-        constant_starts[i+1]=total_len;
-        total_len+=constant_lengths[i+1];
-    }
-
-    return;
-}
-
 /* Searches a sequence and does some kind of operation to "OP". */
 
-template<size_t nvariable, class OP>
+template<size_t nvariable, class OP, size_t nconstant=nvariable+1>
 bool search_sequence_internal(const char* seq, size_t len, 
-    const std::vector<const sequence_dictionary*>& variable_dicts,
-    const std::vector<std::u32string>& constant_hash,
-    const std::vector<int>& variable_lengths,
-    const std::vector<int>& constant_lengths,
-    const std::vector<size_t>& variable_starts,
-    const std::vector<size_t>& constant_starts,
+        
+    const std::array<const sequence_dictionary*, nvariable>& variable_dicts,
+    const std::array<int, nvariable>& variable_lengths,
+    const std::array<size_t, nvariable>& variable_starts,
+
+    const std::array<std::u32string, nconstant>& constant_hash,
+    const std::array<int, nconstant>& constant_lengths,
+    const std::array<size_t, nconstant>& constant_starts,
+
     const size_t total_len,
     OP& operation) 
 {
@@ -121,19 +121,15 @@ bool search_sequence_internal(const char* seq, size_t len,
         return false;
     } 
 
-    constexpr size_t nconstant=nvariable+1;
-
     // Setting up the scanners for a perfect match or single mismatch.
-    std::vector<hash_scanner> constant_scan;
-    constant_scan.reserve(nconstant);
+    std::array<hash_scanner, nconstant> constant_scan;
     for (size_t j=0; j<nconstant; ++j) {
-        constant_scan.push_back(hash_scanner(seq+constant_starts[j], constant_lengths[j]));
+        constant_scan[j]=hash_scanner(seq+constant_starts[j], constant_lengths[j]);
     }
 
-    std::vector<hash_scanner> variable_scan;
-    variable_scan.reserve(nvariable);
+    std::array<hash_scanner, nvariable> variable_scan;
     for (size_t j=0; j<nvariable; ++j) {
-        variable_scan.push_back(hash_scanner(seq+variable_starts[j], variable_lengths[j]));
+        variable_scan[j]=hash_scanner(seq+variable_starts[j], variable_lengths[j]);
     }
 
     // Traversing through the sequence.
@@ -199,7 +195,7 @@ bool search_sequence_internal(const char* seq, size_t len,
     } while (1);
 }
 
-template<size_t nvariable, class OP>
+template<size_t nvariable, class OP, size_t nconstant=nvariable+1>
 bool search_sequence(const char* seq, size_t len, const search_info<nvariable>& info, OP& operation) 
 {
     const auto& perfect=info.perfect;
@@ -212,14 +208,13 @@ bool search_sequence(const char* seq, size_t len, const search_info<nvariable>& 
     size_t total_len=info.total_len;
     bool allow_del=info.allow_del;
 
-    std::vector<const sequence_dictionary*> ptrs(nvariable);
+    std::array<const sequence_dictionary*, nvariable> ptrs;
     for (size_t i=0; i<nvariable; ++i) { ptrs[i]=&(perfect[i]); }
 
     // Perfect matches or mismatch.
     if (search_sequence_internal<nvariable>(seq, len, 
-        ptrs, constant_hash, 
-        variable_lengths, constant_lengths,
-        variable_starts, constant_starts,
+        ptrs, variable_lengths, variable_starts, 
+        constant_hash, constant_lengths, constant_starts,
         total_len, operation)) 
     {
         return true;
@@ -235,7 +230,7 @@ bool search_sequence(const char* seq, size_t len, const search_info<nvariable>& 
     auto cstart=constant_starts;
 
     for (size_t i=1; i<nvariable; ++i) { --vstart[i]; } // Preparing for deletion in the first variable region.
-    for (size_t i=0; i<nvariable; ++i) { --cstart[i+1]; }
+    for (size_t i=1; i<nconstant; ++i) { --cstart[i]; }
 
     for (size_t i=0; i<nvariable; ++i) {
         if (i) {
@@ -247,7 +242,8 @@ bool search_sequence(const char* seq, size_t len, const search_info<nvariable>& 
         --vlen[i];
 
         if (search_sequence_internal<nvariable>(seq, len, 
-            ptrs, constant_hash, vlen, clen, vstart, cstart,
+            ptrs, vlen, vstart, 
+            constant_hash, clen, cstart,
             total_len-1, operation))
         {
             return true;
@@ -256,6 +252,7 @@ bool search_sequence(const char* seq, size_t len, const search_info<nvariable>& 
         ptrs[i]=&(perfect[i]);
         ++vlen[i];
     }
+
 
     return false;
 }
